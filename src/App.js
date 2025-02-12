469b59b1083a05c47bcdb0f8a231d6ad2a409142
RamyaKasinad <ramyakasinadhuni2work@gmail.com>
2025-02-12 18:38:56 +0530

    Updated App.js to check Amplify auto deployment

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My React App 🚀</h1>
+      <h2>Auto Deployment with AWS Amplify is Working! ✅</h2>
     </div>
   );
 }
