2b48073b686a39db04bb180989e0f95bc42525db
RamyaKasinad <ramyakasinadhuni2work@gmail.com>
2025-02-12 20:47:02 +0530

    Testing with hosting branch for deployment

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Welcome to My React App 🚀</h1>
-      <h2>Auto Deployment with AWS Amplify is Working! ✅</h2>
+      <h2>Auto Deployment with AWS Amplify with hostingbranch is Working! ✅</h2>
     </div>
   );
 }
